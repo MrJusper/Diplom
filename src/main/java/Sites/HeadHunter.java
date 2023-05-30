a7e1910de99d443ee98205ede0c1b55830ac33e1
Roman <vip.jusper@mail.ru>
2023-05-30 20:07:24 +0300

    Gavno but worked sdf

--- src/main/java/Sites/HeadHunter.java
+++ src/main/java/Sites/HeadHunter.java
@@ -13,12 +13,12 @@ public class HeadHunter implements Site {
 
     private String search_value = "https://api.hh.ru/vacancies";
     private String answer = "";
-    public String getJsonVacanciesFromRequest(String request) throws IOException {
+    public String getJsonVacanciesFromRequest(String request) throws IOException { // send request and get json
         URL url = new URL(request);
         return IOUtils.toString(url, StandardCharsets.UTF_8);
     }
 
-    void useFilter(){
+    void useFilter(){ //  use user filters
         if(Collector.getKeyWords() != null ){
             search_value = addOrReplacementParameterURL(search_value, "text", Collector.getKeyWords());
         }
@@ -26,7 +26,7 @@ public class HeadHunter implements Site {
         search_value = addOrReplacementParameterURL(search_value, "per_page", "100");
     }
 
-    void fillAnswer(String raw_json_vacancies) throws IOException {
+    void fillAnswer(String raw_json_vacancies) throws IOException { // create list vacancies
         JSONObject json = new JSONObject(raw_json_vacancies);
         int pages = json.getInt("pages");
         int per_page = json.getInt("per_page");
@@ -42,15 +42,15 @@ public class HeadHunter implements Site {
     String makeVacancyString(JSONObject json){
         String vacancy = "";
         vacancy += json.optString("name", "не указано"); //name
-        vacancy += "\t"+json.getJSONObject("salary").optString("from", "не указано");   // salary
-        vacancy += "\t"+json.getJSONObject("salary").optString("to", "не указано");   // salary
+        vacancy += "\t"+json.getJSONObject("salary").optString("from", "не указано");   // salary min
+        vacancy += "\t"+json.getJSONObject("salary").optString("to", "не указано");   // salary max
         vacancy += "\t"+json.getJSONObject("salary").optString("currency", "не указано"); // currency
         vacancy += "\t"+json.getString("alternate_url");    // link
         return vacancy.strip() + "\n";
     }
 
     @Override
-    public final String getParseData() throws IOException {
+    public final String getParseData() throws IOException { // main scenario
         useFilter();
         fillAnswer(getJsonVacanciesFromRequest(search_value));
         System.out.println(answer);
