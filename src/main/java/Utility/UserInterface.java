d293f76478e38cf3089dfab3328572169d07dcbc
Roman <vip.jusper@mail.ru>
2023-05-30 22:11:38 +0300

    Gavno but worked sdf

--- src/main/java/Utility/UserInterface.java
+++ src/main/java/Utility/UserInterface.java
@@ -2,9 +2,18 @@ package Utility;
 
 // Визуальное представление кнопок и ползунков для сбора информации по интересующим вакансиям
 
+import javax.swing.*;
+
 public class UserInterface {
 
     public UserInterface(){
+
         Collector.setKeyWords("Мэнеджер по продажам косметики");
+        makeScreen();
+    }
+
+    void makeScreen(){
+        JFrame frame = new JFrame("Denis");
+        frame.setVisible(true);
     }
 }
