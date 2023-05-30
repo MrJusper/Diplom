1e9cf886250fe8c85b50cef21b417c4a7fa1a1ef
Roman <vip.jusper@mail.ru>
2023-05-30 17:49:17 +0300

    Gavno but worked sdf

--- src/main/java/Sites/HeadHunter.java
+++ src/main/java/Sites/HeadHunter.java
@@ -20,10 +20,10 @@ public class HeadHunter implements Site {
 
     void useFilter(){
         if(Collector.getKeyWords() != null ){
-            search_value = addOrReplacementParametrURL(search_value, "text", Collector.getKeyWords());
+            search_value = addOrReplacementParameterURL(search_value, "text", Collector.getKeyWords());
         }
-        search_value = addOrReplacementParametrURL(search_value, "only_with_salary", "true");
-        search_value = addOrReplacementParametrURL(search_value, "per_page", "100");
+        search_value = addOrReplacementParameterURL(search_value, "only_with_salary", "true");
+        search_value = addOrReplacementParameterURL(search_value, "per_page", "100");
         System.out.println(search_value);
     }
 
@@ -50,23 +50,4 @@ public class HeadHunter implements Site {
         return answer;
     }
 
-    String addOrReplacementParametrURL(String url, String param, String value){
-        value = value.strip().replaceAll(" ","+");
-        if(!(url.contains("?"))){
-            url += "?" + param + "=" + value;
-            return url;
-        }
-        String[] parameters = url.split("\\?")[1].split("&"); // for_example parameters[0] is page=4
-
-        for(int i = 0; i < parameters.length; i++){ // Replace
-            if(parameters[i].startsWith(param)){
-                parameters[i] = param + "=" + value;
-                url = url.split("\\?")[0] + "?" + String.join("&",parameters);
-                return url;
-            }
-        }
-
-        url = url.split("\\?")[0] + "?" + String.join("&",parameters) + "&" + param + "=" + value;
-        return url;
-    }
 }
--- src/main/java/Utility/Site.java
+++ src/main/java/Utility/Site.java
@@ -4,4 +4,24 @@ import java.io.IOException;
 
 public interface Site {
     String getParseData() throws IOException;
+
+    default String addOrReplacementParameterURL(String url, String param, String value){
+        value = value.strip().replaceAll(" ","+");
+        if(!(url.contains("?"))){
+            url += "?" + param + "=" + value;
+            return url;
+        }
+        String[] parameters = url.split("\\?")[1].split("&"); // for_example parameters[0] is page=4
+
+        for(int i = 0; i < parameters.length; i++){ // Replace
+            if(parameters[i].startsWith(param)){
+                parameters[i] = param + "=" + value;
+                url = url.split("\\?")[0] + "?" + String.join("&",parameters);
+                return url;
+            }
+        }
+
+        url = url.split("\\?")[0] + "?" + String.join("&",parameters) + "&" + param + "=" + value;
+        return url;
+    }
 }
